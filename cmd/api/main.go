e328f8a6c2fb925f9a82b86fa15b226dd4ce9550
Joanne Lin <y3933y3933@gmail.com>
2025-04-04 21:03:06 +0800

    feat: add simple server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type config struct {
+	port int
+}
+
 func main() {
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.Parse()
+
+	mux := http.NewServeMux()
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.port),
+		Handler: mux,
+	}
+
+	log.Printf("starting server :%d\n", cfg.port)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
