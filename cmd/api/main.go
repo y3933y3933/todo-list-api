aa821d6940117e304557af780ee8d44ec6cebbd6
Joanne Lin <y3933y3933@gmail.com>
2025-04-04 21:12:34 +0800

    feat: use slog package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
+
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 type config struct {
@@ -16,16 +18,21 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.Parse()
 
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+	slog.SetDefault(logger)
+
 	mux := http.NewServeMux()
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%d", cfg.port),
 		Handler: mux,
 	}
 
-	log.Printf("starting server :%d\n", cfg.port)
+	slog.Info("starting server", "addr", srv.Addr)
 	err := srv.ListenAndServe()
 	if err != nil {
-		log.Fatal(err)
+		slog.Error(err.Error())
 	}
 
 }
