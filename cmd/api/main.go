aa9a67089417f2d5359ff5d38df0caf1e666108e
Joanne Lin <y3933y3933@gmail.com>
2025-04-05 14:01:16 +0800

    feat: install postgresql driver & connect to db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,32 +1,51 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
+	"time"
 
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 type config struct {
 	port int
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
 	logger *slog.Logger
+	config config
 }
 
 func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://todos:todos@localhost/todos?sslmode=disable", "PostgreSQL DSN")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		Level: slog.LevelDebug,
 	}))
 
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+	logger.Info("database connection pool established")
+
 	app := application{
+		config: cfg,
 		logger: logger,
 	}
 
@@ -36,9 +55,33 @@ func main() {
 	}
 
 	slog.Info("starting server", "addr", srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		slog.Error(err.Error())
 	}
+
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	db.SetConnMaxLifetime(0)
+	db.SetMaxIdleConns(3)
+	db.SetMaxOpenConns(3)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 
 }
